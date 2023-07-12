635c91f624ae33b770c9f34e48887ad5c25569b1
AsisNath <nathasiskumar@yahoo.com>
2023-07-12 12:04:40 -0500

    Create test_square.py

---
+++ test_square.py
@@ -0,0 +1,14 @@
+import unittest
+
+class SquareTestCase(unittest.TestCase):
+    def test_positive_number(self):
+        self.assertEqual(square(5), 25)
+    
+    def test_negative_number(self):
+        self.assertEqual(square(-3), 9)
+    
+    def test_zero(self):
+        self.assertEqual(square(0), 0)
+
+if __name__ == '__main__':
+    unittest.main()
